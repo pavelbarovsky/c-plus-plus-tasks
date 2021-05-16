827ff507654dee6c7755b8ed4c68d017337eab61
pavelbarovsky <pashazhar111@mail.ru>
2021-05-16 10:43:20 +0300

    ++func

--- week1/2/main.cpp
+++ week1/2/main.cpp
@@ -6,6 +6,16 @@
  Вариант 3, периметр и площадь квадрата (номер 2с)
 */
 
+float num1;
+
+void per() {
+std::cout   << "Perimetr = " << num1 + num1 << std::endl;
+}
+
+void plosh(){
+std::cout << "Ploshad = " << num1 * num1 << std::endl;
+}
+
 int main()
 {
 
@@ -14,22 +24,17 @@ int main()
     fin.open("in.txt");
     fout.open("out.txt");
 
-    float num1;
 
     std::cout << "Vvedite storonu kvadrata: ";
     std::cin >> num1;
     fin >> num1;
     fin.close();
 
-    float P = num1 + num1;
-    float S = num1 * num1;
-    fout << P << ' ' << S;
+per();
+plosh();
+
     fout.close();
 
-
-    std::setprecision(7);
-    std::cout   << "Perimetr = " << P << std::endl
-                << "Ploshad = " << S << std::endl;
 
  return 0;
 
--- week1/3/main.cpp
+++ week1/3/main.cpp
@@ -7,36 +7,38 @@
 числа. Вариант 3, задача 6
 */
 
-int main()
-{
-    std::ifstream fin;
-    std::ofstream fout;
-    fin.open("in.txt");
-    fout.open("out.txt");
+float num1;
+float num2;
+float num3;
 
-    float num1;
-    float num2;
-    float num3;
+void sred() {
+    std::cout << "(x + y + z)/3, (Srednee) = " << (num1 + num2 + num3)/3 << std::endl;
+}
 
-    fin >> num1;
-    fin >> num2;
-    fin >> num3;
-    fin.close();
+void xz() {
+     std::cout << "2*(x + z)-3*y, (Raznost) = " << 2*(num1 + num3)-3*num2 << std::endl;
+}
+int main() {
+
+std::ifstream fin;
+std::ofstream fout;
+fin.open("in.txt");
+fout.open("out.txt");
+
+fin >> num1;
+fin >> num2;
+fin >> num3;
+fin.close();
 
     std::cout << "Enter three numbers: ";
     std::cin >> num1 >> num2 >> num3;
-    float S = (num1 + num2 + num3)/3;
-    float R = 2*(num1 + num3)-3*num2;
 
-    fout << S << ' ' << R;
-    fout.close();
+    sred();
+    xz();
 
+fout << (num1 + num2 + num3)/3 << ' ' << 2*(num1 + num3)-3*num2;
+fout.close();
 
-    std::setprecision(7);
-    std::cout   << "(num1 + num2 + num3)/3, (Srednee) = " << S << std::endl
-                << "2*(num1 + num3)-3*num2, (Raznost) = " << R << std::endl;
-
-
- return 0;
+return 0;
 
 }
